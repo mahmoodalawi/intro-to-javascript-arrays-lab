56cfbe2a6dad1ca87074f39acd32ecfc47b90007
mahmoodalawi <mahmoodbh19@email.com>
2025-04-27 01:26:37 +0300

    ex 2 & 3

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-/*
-Exercise 1: Define an empty array
-
-1) Create an empty array and assign it to a variable called `foods`.
-
-Exercise 1 has been completed for you:
-*/
+// Exercise 1 from the site  
 
 const foods = [];  
 
 console.log('Exercise 1 result:', foods);
+
+// Exercise 2 with push 
+const foods = [];
+
+foods.push('pizza' , 'cheeseburger');
+
+console.log('Exercise 2 result:', foods);
+
+// Exercise 3
+const foods = [];
+
+foods.unshift('taco');
+
+console.log('Exercise 3 result:', foods);

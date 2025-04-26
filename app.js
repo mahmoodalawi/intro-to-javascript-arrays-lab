61b0329d8523fbaf064e6ee84f8d5380effef4f0
mahmoodalawi <mahmoodbh19@email.com>
2025-04-27 01:44:40 +0300

    ex 5 updated

--- app.js
+++ app.js
@@ -19,8 +19,19 @@ foods.unshift('taco');
 console.log('Exercise 3 :', foods);
 
 
+// Exercise 4
+
 const foods = ['taco', 'pizza', 'cheeseburger'];
 
 const favFood = foods[1];
 
 console.log('Exercise 4:', favFood);
+
+// Exercise 5
+
+const foods = ['pizza', 'cheeseburger'];
+
+
+foods.splice(1, 0, 'tofu');
+
+console.log('Exercise 5 result:', foods);
